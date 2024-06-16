3764eb32c9c956272b57434fa7da7959d0933a82
oGabrielBR <gabrielvfx17@gmail.com>
2024-06-16 12:29:24 -0300

    segundo commit

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -22,7 +22,7 @@ class _QuizState extends State<Quiz> {
       "alternativa_correta": 3,
       "imagem": "assets/Taça.jpg",
       "altura_imagem": 200.0,
-      "cor_borda": Color.fromARGB(255, 243, 170, 33), // Cor da borda para esta imagem
+      "cor_borda": Color.fromARGB(255, 243, 170, 33),
     },
     {
       "pergunta": "Qual jogador é conhecido como 'Slim Reaper' na NBA?",
@@ -35,7 +35,7 @@ class _QuizState extends State<Quiz> {
       "alternativa_correta": 3,
       "imagem": "assets/Reaper.jpg",
       "altura_imagem": 180.0,
-      "cor_borda": const Color.fromARGB(255, 0, 0, 0), // Cor da borda para esta imagem
+      "cor_borda": const Color.fromARGB(255, 0, 0, 0),
     },
     {
       "pergunta": "Qual jogador é conhecido como 'The Don' na NBA?",
@@ -48,7 +48,7 @@ class _QuizState extends State<Quiz> {
       "alternativa_correta": 1,
       "imagem": "assets/Screenshot_2.png",
       "altura_imagem": 180.0,
-      "cor_borda": Color.fromARGB(255, 4, 0, 255), // Cor da borda para esta imagem
+      "cor_borda": Color.fromARGB(255, 4, 0, 255),
     },
     {
       "pergunta": "Qual o maior arremessador da linha de 3 pontos da NBA?",
@@ -61,7 +61,7 @@ class _QuizState extends State<Quiz> {
       "alternativa_correta": 0,
       "imagem": "assets/NBA-3-Point-Shooting.png",
       "altura_imagem": 150.0,
-      "cor_borda": const Color.fromARGB(255, 255, 217, 0), // Cor da borda para esta imagem
+      "cor_borda": const Color.fromARGB(255, 255, 217, 0),
     },
     {
       "pergunta": "Quantos títulos tem Michael Jordan?",
@@ -74,7 +74,7 @@ class _QuizState extends State<Quiz> {
       "alternativa_correta": 0,
       "imagem": "assets/michael-jordan.png",
       "altura_imagem": 180.0,
-      "cor_borda": Color.fromARGB(255, 255, 0, 0), // Cor da borda para esta imagem
+      "cor_borda": Color.fromARGB(255, 255, 0, 0),
     },
   ];
 
@@ -113,74 +113,80 @@ class _QuizState extends State<Quiz> {
             ],
           ),
         ),
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: _perguntaNumero < _quiz.length
-              ? Card(
-                  elevation: 5,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Text(
-                          'Pergunta ${_perguntaNumero + 1} de ${_quiz.length}',
-                          style: TextStyle(fontSize: 18),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Text(
-                          _quiz[_perguntaNumero]['pergunta'],
-                          style: TextStyle(fontSize: 20),
-                        ),
-                      ),
-                      Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12),
-                        border: Border.all(
-                          color: _quiz[_perguntaNumero]['cor_borda'] ?? Colors.black,
-                          width: 3,
-                        )
-                        ),
-                        height: _quiz[_perguntaNumero]['altura_imagem'],
-                        margin: EdgeInsets.symmetric(horizontal: 110),
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(10),
-                          child: Image.asset(
-                            _quiz[_perguntaNumero]['imagem'],
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 20),
-                      Column(
-                        children: (_quiz[_perguntaNumero]['respostas'] as List<String>)
-                            .map((resposta) {
-                          return Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                            child: ElevatedButton(
-                              onPressed: () => _responder(
-                                  _quiz[_perguntaNumero]['respostas'].indexOf(resposta)),
-                              style: ElevatedButton.styleFrom(
-                                foregroundColor: Colors.black, backgroundColor: Colors.white,
-                                padding: EdgeInsets.all(12),
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(10),
-                                ),
-                              ),
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            return SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: _perguntaNumero < _quiz.length
+                    ? Card(
+                        elevation: 5,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(16.0),
                               child: Text(
-                                resposta,
+                                'Pergunta ${_perguntaNumero + 1} de ${_quiz.length}',
                                 style: TextStyle(fontSize: 18),
                               ),
                             ),
-                          );
-                        }).toList(),
-                      ),
-                    ],
-                  ),
-                )
-              : Center(child: CircularProgressIndicator()),
+                            Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Text(
+                                _quiz[_perguntaNumero]['pergunta'],
+                                style: TextStyle(fontSize: 20),
+                              ),
+                            ),
+                            Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(12),
+                                border: Border.all(
+                                  color: _quiz[_perguntaNumero]['cor_borda'] ?? Colors.black,
+                                  width: 3,
+                                )
+                              ),
+                              height: _quiz[_perguntaNumero]['altura_imagem'],
+                              margin: EdgeInsets.symmetric(horizontal: constraints.maxWidth * 0.15),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(10),
+                                child: Image.asset(
+                                  _quiz[_perguntaNumero]['imagem'],
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 20),
+                            Column(
+                              children: (_quiz[_perguntaNumero]['respostas'] as List<String>)
+                                  .map((resposta) {
+                                return Padding(
+                                  padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                                  child: ElevatedButton(
+                                    onPressed: () => _responder(
+                                        _quiz[_perguntaNumero]['respostas'].indexOf(resposta)),
+                                    style: ElevatedButton.styleFrom(
+                                      foregroundColor: Colors.black, backgroundColor: Colors.white,
+                                      padding: EdgeInsets.all(12),
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(10),
+                                      ),
+                                    ),
+                                    child: Text(
+                                      resposta,
+                                      style: TextStyle(fontSize: 18),
+                                    ),
+                                  ),
+                                );
+                              }).toList(),
+                            ),
+                          ],
+                        ),
+                      )
+                    : Center(child: CircularProgressIndicator()),
+              ),
+            );
+          },
         ),
       ),
     );
